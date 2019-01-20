f99c32dd37c4229d0ac44614c8072a569df27a0d
IbragimovAdel <ibragimovadel2001@gmail.com>
2019-01-20 20:31:04 +0300

    Update

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,31 @@
-$(function(){
+$(function () {
 
-    var stockSwiper = new Swiper($("#stocks"),{
+    var stockSwiper = new Swiper($("#stocks"), {
         spaceBetween: 30,
         centeredSlides: true,
         autoplay: {
-          delay: 5000,
-          disableOnInteraction: false,
+            delay: 5000,
+            disableOnInteraction: false,
         },
         pagination: {
-          el: $("#stocks .swiper-pagination"),
-          clickable: true,
+            el: $("#stocks .swiper-pagination"),
+            clickable: true,
         },
         navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
+            nextEl: $("#stocks .swiper-button-next"),
+            prevEl: $("#stocks .swiper-button-prev"),
         },
-      })
+    })
+
+    var service = $('.services .service')
+    service.hover( function () {
+        $(this).find('i').animate({opacity: 1});
+        $(this).find('.content').animate({opacity: 0});
+    },
+    function () {
+        $(this).find('i').animate({opacity: 0});
+        $(this).find('.content').animate({opacity: 1});
+    })
+
 
 })
