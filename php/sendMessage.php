2c626950f1b81e9f43b6e02eec39f880a77422d9
IbragimovAdel <ibragimovadel2001@gmail.com>
2019-02-03 21:49:20 +0300

    Update sendMessage.php

--- php/sendMessage.php
+++ php/sendMessage.php
@@ -2,7 +2,7 @@
 
 $name = $_POST['user_name'];
 $phone = $_POST['user_phone'];
-$carmodel = $_POST['user_carmodel']
+$carmodel = $_POST['user_carmodel'];
 
 $token = "733330102:AAGZ5YbIxj7umx_pv9q3GCUYJmBJGH-jCeo";
 $chat_id = "259993783";
@@ -15,15 +15,15 @@ $arr = array(
 
 foreach($arr as $key => $value){
     $txt .= "<b>".$key."</b>".$value."%0A";
-}
+};
 
-$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r")
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 
 if($sendToTelegram){
-    header('Location: index.html')
+    header('Location: index.html');
     return true;
 } else {
-    header('Location: index.html')
+    header('Location: index.html');
     return false;
 }
 
