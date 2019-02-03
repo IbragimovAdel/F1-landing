c557389d641f79b83a4ee267148406dde20d1d77
IbragimovAdel <ibragimovadel2001@gmail.com>
2019-02-03 21:36:44 +0300

    Test

--- php/sendMessage.php
+++ php/sendMessage.php
@@ -1,3 +1,30 @@
 <?php
 
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$carmodel = $_POST['user_carmodel']
+
+$token = "733330102:AAGZ5YbIxj7umx_pv9q3GCUYJmBJGH-jCeo";
+$chat_id = "259993783";
+
+$arr = array(
+    'Имя: ' => $name,
+    'Телефон: ' => $phone,
+    'Марка автомобиля: ' => $carmodel
+);
+
+foreach($arr as $key => $value){
+    $txt .= "<b>".$key."</b>".$value."%0A";
+}
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r")
+
+if($sendToTelegram){
+    echo '<h1>Спасибо!</h1>'
+    return true;
+} else {
+    echo '<h1>Ошибка!</h1>'
+    return false;
+}
+
 ?>
