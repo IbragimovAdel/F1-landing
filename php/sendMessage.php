f355fa8e673f0bfa2a7b8fc91e79f062b04ec161
IbragimovAdel <ibragimovadel2001@gmail.com>
2019-02-04 16:35:47 +0300

    Second Form

--- php/sendMessage.php
+++ php/sendMessage.php
@@ -7,6 +7,8 @@ $carmodel = $_POST['user_carmodel'];
 $token = "733330102:AAGZ5YbIxj7umx_pv9q3GCUYJmBJGH-jCeo";
 $chat_id = "259993783";
 
+$txt = "";
+
 $arr = array(
     'Имя: ' => $name,
     'Телефон: ' => $phone,
@@ -17,6 +19,7 @@ foreach($arr as $key => $value){
     $txt .= "<b>".$key."</b>".$value."%0A";
 };
 
+mail("ibragimovadel2001@gmail.com","Заявка с сайта","Имя: ".$name.". Телефон: ".$phone.". Марка автомобиля: ".$carmodel);
 $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 
 if($sendToTelegram){
