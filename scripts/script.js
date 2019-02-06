5464c05c56edd25102b98fd687b91a67e7e2c098
IbragimovAdel <ibragimovadel2001@gmail.com>
2019-02-06 18:35:06 +0300

    Update

--- scripts/script.js
+++ scripts/script.js
@@ -19,8 +19,9 @@ $(function () {
 
     var stockSwiper = new Swiper($("#stocks"), {
         slidesPerView: 1,
-        spaceBetween: 30,
+        spaceBetween: 0,
         centeredSlides: true,
+        loop: true,
         autoplay: {
             delay: 5000,
             disableOnInteraction: false,
